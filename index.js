ea1cec99ccb6e0a5f8aeb312454ef5506dea408e
Rahul Somaraj <torahulsomaraj@gmail.com>
2020-06-04 20:06:51 +0530

    change in model

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 //variables start over here
-const PORT = porcess.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use('/login',login_routes);
 
