780df00c3d20c48c37bfaa0154da24740e3f86be
Rahul Somaraj <torahulsomaraj@gmail.com>
2020-06-04 16:22:52 +0530

    intial setup first feature index file and routes

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+//intalize routes 
+const user_routes = require('./routes/user_routes');
+const login_routes = require('./routes/login.routes');
+
+//app starts over here 
+const app = express();
+app.use(express.json());
+
+//variables start over here
+const PORT = porcess.env.PORT || 3000;
+
+app.use('/user',user_routes);
+app.use('/login',login_routes);
+
+app.listen(PORT,()=>{
+    console.log("Server Started")
+})
