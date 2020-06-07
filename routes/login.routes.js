d0a03ca56009068b5bd429a2dfd45e957337b2c0
Rahul Somaraj <torahulsomaraj@gmail.com>
2020-06-08 00:23:34 +0530

    change in login module added required fields

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const loginService = require('../services/login.service')
+const path = require('path');
 
 router.use(express.json());
 
@@ -8,7 +9,7 @@ router.post('/login',(request,response)=>{
     loginService.login(JSON.parse(JSON.stringify(request.body))).then((data)=>{
         console.log("saved")
         if(!!data){
-            response.render(__dirname+'../public/JobHunt/candidates_dashboard.ejs',{data :data})
+            response.render(path.join(__dirname,'../public/JobHunt/candidates_dashboard.ejs'),{data :data})
         }
     })
     .catch((err)=>{
