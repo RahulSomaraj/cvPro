b74fbaf3e6a57e5685bed53b97cec0a68689653d
JK <jerinisready@gmail.com>
2020-06-14 03:02:48 +0530

    fixes

--- routes/candidate.routes.js
+++ routes/candidate.routes.js
@@ -7,55 +7,72 @@ var resume = require(JSON.parse(JSON.stringify('../resume.json')))
 const mailer = require('../services/nodemailer');
 router.use(express.json());
 
-var data ;
 
-router.get('/profile',(request,response)=>{
+async function get_context_data(request) {
+    return await userService.findOne({_id : request.cookies['userId']})
+}
+
+router.get('/profile', async (request,response) => {
     console.log(request.cookies['userId']);
-    response.render(path.join(__dirname,'../public/JobHunt/candidates_profile.ejs'),{data : data});
+    var data = await get_context_data(request);
+    response.render(path.join(__dirname,'../public/JobHunt/candidates_profile.ejs'), {data : data});
 });
-router.get('/dashboard',async (request,response)=>{
-    data = await userService.findOne({_id : request.cookies['userId']})
-    response.render(path.join(__dirname,'../public/JobHunt/candidates_dashboard'),{data : data});
+
+router.get('/dashboard',async (request,response) => {
+    var data = await get_context_data(request);
+    response.render(path.join(__dirname,'../public/JobHunt/candidates_dashboard'), {data : data});
 });
-router.get('/change_password',(request,response)=>{
-    response.render(path.join(__dirname,'../public/JobHunt/candidates_change_password.ejs'),{data : data});
+
+router.get('/change_password', async (request,response) => {
+    var data = await get_context_data(request);
+    response.render(path.join(__dirname,'../public/JobHunt/candidates_change_password.ejs'), {data : data});
 });
-router.get('/myresume',(request,response)=>{
-    var data = await userService.findOne({_id : request.cookies['userId']})
-    response.render(path.join(__dirname,'../public/JobHunt/candidates_my_resume.ejs'),{data : data});
+
+router.get('/myresume', async (request,response) => {
+    var data = await get_context_data(request);
+    response.render(path.join(__dirname,'../public/JobHunt/candidates_my_resume.ejs'), {data : data});
 });
-router.get('/shortList',(request,response)=>{
-    response.render(path.join(__dirname,'../public/JobHunt/candidates_shortlist.ejs'),{data : data});
+
+router.get('/shortList', async (request,response) => {
+    var data = await get_context_data(request);
+    response.render(path.join(__dirname,'../public/JobHunt/candidates_shortlist.ejs'), {data : data});
 });
-router.get('/appliedJobs',(request,response)=>{
-    response.render(path.join(__dirname,'../public/JobHunt/candidates_applied_jobs.ejs'),{data : data});
+
+router.get('/appliedJobs', async (request,response) => {
+    var data = await get_context_data(request);
+    response.render(path.join(__dirname,'../public/JobHunt/candidates_applied_jobs.ejs'), {data : data});
 });
-router.get('/jobAlert',(request,response)=>{
-    response.render(path.join(__dirname,'../public/JobHunt/candidates_job_alert.ejs'),{data : data});
+
+router.get('/jobAlert', async (request, response) => {
+    var data = await get_context_data(request);
+    response.render(path.join(__dirname,'../public/JobHunt/candidates_job_alert.ejs'), {data : data});
 });
-router.get('/coverLetter',(request,response)=>{
-    response.render(path.join(__dirname,'../public/JobHunt/candidates_cv_cover_letter.ejs'),{data : data});
+
+router.get('/coverLetter', async (request,response) => {
+    var data = await get_context_data(request);
+    response.render(path.join(__dirname,'../public/JobHunt/candidates_cv_cover_letter.ejs'), {data : data});
 });
-router.get('/single',(request,response)=>{
-    response.render(path.join(__dirname,'../public/JobHunt/candidates_single.ejs'),{data : {...data,resume:{...resume}}});
+
+router.get('/single', async (request,response) => {
+    var data = await get_context_data(request);
+    response.render(path.join(__dirname,'../public/JobHunt/candidates_single.ejs'), {data : {...data,resume:{...resume}}});
 });
-router.get('/downloadresume',(request,response)=>{
+
+router.get('/downloadresume', async (request,response) => {
     response.download(path.join(__dirname,'resume.pdf'));
     response.redirect("/single");
 });
 
-router.get('/mailAction',(request,response)=>{
+router.get('/mailAction', async (request, response) => {
     mailer.mail(JSON.parse(JSON.stringify(request.body)))
     response.redirect("/single");
 });
 
-
-router.post('/updatecandidate',(request,response)=>{
+router.post('/updatecandidate', async  (request, response)=>{
     console.log(request.cookies['userId']);
     var obj = JSON.parse(JSON.stringify(request.body))
     Object.keys(obj).forEach(key => (obj[key] !== "" ||obj[key]) ? obj[key] = obj[key] : delete obj[key] );
     userService.update({_id : request.cookies['userId'],...obj}).then((data)=>{
-        console.log("saved");
         console.log(data)
         response.render(path.join(__dirname,'../public/JobHunt/candidates_profile.ejs'),{data:data});
     }).catch((err)=>{
@@ -63,19 +80,14 @@ router.post('/updatecandidate',(request,response)=>{
     })
 });
 
-router.post('/api/v1/updateCandidateProfile',(request,response)=>{
-    console.log(request.cookies['userId']);
+
+router.post('/updateCandidateProfile', async (request,response) => {
+    var data = await get_context_data(request);
     var obj = JSON.parse(JSON.stringify(request.body))
-    console.log(obj)
-    // Object.keys(obj).forEach(key => (obj[key] !== "" ||obj[key]) ? obj[key] = obj[key] : delete obj[key] );
-    // userService.update({_id : request.cookies['userId'],...obj}).then((data)=>{
-    //     console.log("saved");
-    //     console.log(data)
-    //     response.render(path.join(__dirname,'../public/JobHunt/candidates_profile.ejs'),{data:data});
-    // }).catch((err)=>{
-    //     console.log(err);
-    // })
 });
 
 
 module.exports =  router;
+
+
+
