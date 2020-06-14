05cee713d73104856b670e25f9280b24475bbd98
JK <jerinisready@gmail.com>
2020-06-14 19:08:33 +0530

    issue fixes in employer

--- routes/employer.routes.js
+++ routes/employer.routes.js
@@ -2,28 +2,30 @@ const express = require('express');
 const router = express.Router();
 const employerService = require('../services/employer.service');
 router.use(express.json());
-
+const userService = require('../services/user.service');
+const path = require('path');
+let resume = {};
 
 async function get_context_data(request) {
-    return await userService.findOne({_id : request.cookies['userId']})
+    return await userService.findOne({_id : request.session.userId})
 }
 
-router.get('list', async (request,response) => {
+router.get('/list', async (request, response) => {
     var data = await get_context_data(request);
     response.render(path.join(__dirname,'../public/JobHunt/employer_list.ejs'), {data : {...data, resume:{...resume}}});
 });
 
-router.get('profile', async (request,response) => {
+router.get('/profile', async (request, response) => {
     var data = await get_context_data(request);
     response.render(path.join(__dirname,'../public/JobHunt/employer_profile.ejs'), {data : {...data, resume:{...resume}}});
 });
 
-router.get('manage-jobs', async (request,response) => {
+router.get('/manage-jobs', async (request, response) => {
     var data = await get_context_data(request);
     response.render(path.join(__dirname,'../public/JobHunt/employer_manage_jobs.ejs'), {data : {...data, resume:{...resume}}});
 });
 
-router.get('resume', async (request,response) => {
+router.get('/resume', async (request,response) => {
     var data = await get_context_data(request);
     response.render(path.join(__dirname,'../public/JobHunt/employer_resume.ejs'), {data : {...data, resume:{...resume}}});
 });
@@ -41,3 +43,5 @@ router.get('/change_password', async (request,response) => {
 
 
 module.exports =  router;
+
+
